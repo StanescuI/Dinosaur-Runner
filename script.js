044ae84518cdd792be6961ae10748ae33a9b25bd
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-05-17 21:52:18 +0300

    Major Changes
    Indentation fix;
    Code clarity and redundant variables removed
    Simplified functions with new logic based on keyframes instead of inline attributes or computed style checking
    Cleaner code and shorter lines

--- script.js
+++ script.js
@@ -1,214 +1,152 @@
-// GAME VARIABLES AND CONSTANTS
+// Constants and variables
 
-const SPAWN_POSITION = 580
-const DESPAWN_POSITION = 0
-const OBSTACLE_WIDTH = 20
-const FLOOR_LEVEL = 100
-const MAX_JUMP = 60
-const FALL_SPEED = 2.5
-let gameSpace = document.getElementById('gameSpace')
-let scoreDisplay = document.getElementById('score')
-let scoreScreen = document.getElementById('scoreScreen')
-let dino = document.querySelector('#dinosaur')
-let dinoStyle = window.getComputedStyle(dino)
-document.addEventListener('keydown', keyPressed)
-document.addEventListener('keyup', keyUnpressed)
-let isJumping = false
-let sneaking = false
-let isPaused = true
-let isFalling = false
-let canJump = true
-let jumpInterval
-let gravityInterval
-let obstacleMoveId
-let obstacleSpawnId
-let jumpTimeout
-let difficultyId
-let scoreId
-let difficultySpike = 1.00
-let spawnRate = 1500
-let scoreAndMotion = 30
-let jumpHeight = 0
-let scoreValue = 0
-let dinoLeft = 40
-resetDino()
+const SPAWN_POSITION = 580;
+const OBSTACLE_WIDTH = 20;
+const ANIMATION_DIFF = 30;
+let gameSpace = document.getElementById('gameSpace');
+let scoreDisplay = document.getElementById('score');
+let scoreScreen = document.getElementById('scoreScreen');
+let dino = document.querySelector('.dinosaur');
+document.addEventListener('keydown', keyPressed);
+document.addEventListener('keyup', keyUnpressed);
+let isJumping = false;
+let sneaking = false;
+let isPaused = true;
+let canJump = true;
+let currentType;
+let difficultyId;
+let checkCollisionId;
+let scoreId;
+let dinoLeft = 40;
+let difficultySpike = 1.00;
+let scoreMultiplier = 50;
+let scoreValue = 0;
 
-// GAME STATES - START / GAMEOVER
-
-let changedTab = document.addEventListener('visibilitychange', gameOver)
+// Game states
 
 function startGame() {
-    obstacleSpawnId = setInterval(createObstacle, (spawnRate * difficultySpike).toFixed(0))
-    obstacleMoveId = setInterval(moveObstacle, (scoreAndMotion * difficultySpike).toFixed(0))
-    scoreId = setInterval(increaseScore, (scoreAndMotion * difficultySpike).toFixed(0))
-    checkCollisionId = setInterval(checkCollision, 10)
-    difficultyId = setInterval(increaseDifficulty, 3000)
-    document.addEventListener('keydown', keyPressed)
-    isPaused = false
+    document.addEventListener('visibilitychange', gameOver);
+    createObstacle();
+    scoreId = setInterval(increaseScore, (scoreMultiplier * difficultySpike).toFixed(0));
+    checkCollisionId = setInterval(checkCollision, 10);
+    difficultyId = setInterval(increaseDifficulty, 3000);
+    isPaused = false;
+    dino.classList.add('default');
 }
 
 function gameOver() {
-    clearInterval(obstacleMoveId)
-    clearInterval(obstacleSpawnId)
-    clearInterval(checkCollisionId)
-    clearInterval(gravityInterval)
-    clearInterval(scoreId)
-    clearInterval(difficultyId)
-    document.removeEventListener('visibilitychange', gameOver)
-    document.removeEventListener('keydown', keyPressed)
-    isPaused = true
-    scoreDisplay.innerHTML = `GAME OVER ! Your final score: ${scoreValue.toFixed(0)}`
-    let restart = document.createElement('button')
-    restart.innerHTML = 'Restart'
-    restart.onclick = restartGame
-    scoreScreen.appendChild(restart)
+    clearInterval(checkCollisionId);
+    clearInterval(scoreId);
+    clearInterval(difficultyId);
+    let animatedObject = document.getElementById('obstacle');
+    let rect = animatedObject.getBoundingClientRect();
+    animatedObject.style.animation = 'none';
+    animatedObject.style.left = `${rect.left - ANIMATION_DIFF}px`;
+    document.removeEventListener('visibilitychange', gameOver);
+    document.removeEventListener('keydown', keyPressed);
+    isPaused = true;
+    scoreDisplay.innerHTML = `GAME OVER ! Your final score: ${scoreValue.toFixed(0)}`;
+    let restart = document.createElement('button');
+    restart.innerHTML = 'Restart';
+    restart.onclick = restartGame;
+    scoreScreen.appendChild(restart);
 }
 
 function restartGame() {
-    location.reload()
+    location.reload();
 }
 
-// OBSTACLE CREATION
+// Game logic and updates
 
 function createObstacle() {
-    let random = Math.floor(Math.random() * 3) + 1
-    let obstacle = document.createElement('div')
-    obstacle.classList.add(`obstacle${random}`)
-    obstacle.classList.add('obstacle')
-    obstacle.id = `${(Math.random() * 30).toFixed(5)}`
-    gameSpace.appendChild(obstacle)
-}
-
-// GAME UPDATE - MOVEMENT AND COLLISION
-
-function moveObstacle() {
-    let obstacles = document.querySelectorAll('.obstacle')
-    obstacles.forEach(function(obstacle) {
-        let obstacleStyle = window.getComputedStyle(obstacle)
-        let obstacleLeft = parseInt(obstacleStyle.getPropertyValue('left'))
-        if (obstacleLeft > 0) {
-            obstacleLeft -= 5
-            obstacle.style.left = `${obstacleLeft}px`
-        } else {
-            obstacle.remove()
-        }
-    })
+    let random = Math.floor(Math.random() * 3) + 1;
+    let obstacle = document.createElement('div');
+    obstacle.classList.add(`obstacle${random}`);
+    currentType = random;
+    obstacle.id = 'obstacle';
+    obstacle.style.animation = `move ${difficultySpike * 4}s linear infinite`;
+    gameSpace.appendChild(obstacle);
+    obstacle.addEventListener('animationiteration', function() {
+        obstacle.style.animation = 'none';
+        random = Math.floor(Math.random() * 3) + 1;
+        obstacle.classList.remove(`obstacle${currentType}`);
+        obstacle.classList.add(`obstacle${random}`);
+        currentType = random;
+        void obstacle.offsetWidth;
+        obstacle.style.animation = `move ${difficultySpike * 4}s linear infinite`;
+    });
 }
 
 function checkCollision() {
-    let obstacles = document.querySelectorAll('.obstacle')
-    let dinoTop = parseInt(dinoStyle.getPropertyValue('top'))
-    let dinoHeight = parseInt(dinoStyle.getPropertyValue('height'))
-    let dinoBottom = dinoTop + dinoHeight
-    let dinoRight = parseInt(dinoStyle.getPropertyValue('width')) + dinoLeft
-    obstacles.forEach(function(obstacle) {
-        let obstacleStyle = window.getComputedStyle(obstacle)
-        let obstacleLeft = parseInt(obstacleStyle.getPropertyValue('left'))
-        let obstacleHeight = parseInt(obstacleStyle.getPropertyValue('height'))
-        let obstacleRight = obstacleLeft + OBSTACLE_WIDTH
-        let obstacleTop = parseInt(obstacleStyle.getPropertyValue('top'))
-        let obstacleBottom = obstacleTop + obstacleHeight
-        if (obstacleLeft <= dinoRight && obstacleRight >= dinoLeft &&
-            obstacleBottom >= dinoTop && obstacleTop <= dinoBottom) {
-            gameOver()
-        }
-    })
+    let obstacle = document.querySelector('#obstacle');
+    let obstaclePos = obstacle.getBoundingClientRect();
+    let dinoPos = dino.getBoundingClientRect();
+    if (obstaclePos.left <= dinoPos.right &&
+        obstaclePos.right >= dinoPos.left &&
+        obstaclePos.bottom >= dinoPos.top &&
+        obstaclePos.top <= dinoPos.bottom) {
+        gameOver();
+    }
 }
 
 function increaseScore() {
-    scoreValue += 0.2
-    scoreDisplay.textContent = `Score : ${scoreValue.toFixed(0)}`
+    scoreValue += 0.2;
+    scoreDisplay.textContent = `Score : ${scoreValue.toFixed(0)}`;
 }
 
 function increaseDifficulty() {
-    if (difficultySpike > 0.1) {
-        difficultySpike -= 0.05
-        clearInterval(obstacleMoveId)
-        clearInterval(obstacleSpawnId)
-        clearInterval(scoreId)
-        obstacleSpawnId = setInterval(createObstacle, (spawnRate * difficultySpike).toFixed(0))
-        obstacleMoveId = setInterval(moveObstacle, (scoreAndMotion * difficultySpike).toFixed(0))
-        scoreId = setInterval(increaseScore, (scoreAndMotion * difficultySpike).toFixed(0))
+    if (difficultySpike > 0.5) {
+        difficultySpike -= 0.05;
+        clearInterval(scoreId);
+        scoreId = setInterval(increaseScore, (scoreMultiplier * difficultySpike).toFixed(0));
     }
 }
 
+// User input and dinosaur style changes
+
 function keyPressed(e) {
-    e.preventDefault()
-    if (!isJumping && e.key === 'ArrowUp' && parseInt(dinoStyle.getPropertyValue('top')) >= 100) {
-        isJumping = true
-        jump()
+    if (e.key === 'ArrowUp' && !isJumping) {
+        jump();
     }
     if (e.key === 'ArrowDown') {
-        sneaking = true
-        sneak()
+        sneaking = true;
+        sneak();
     }
 }
 
 function keyUnpressed(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (e.key === 'ArrowUp') {
-        clearInterval(jumpInterval)
-        startGravity()
-        canJump = true
+        canJump = true;
     }
     if (e.key === 'ArrowDown') {
-        sneaking = false
-        resetDino()
+        sneaking = false;
+        resetDino();
     }
 }
 
 function jump() {
-    if (!sneaking && canJump) {
-        if (isPaused) {
-            startGame()
-        }
-        jumpInterval = setInterval(function() {
-            if (jumpHeight < MAX_JUMP) {
-                let dinoTop = parseInt(dinoStyle.getPropertyValue('top'))
-                dinoTop -= 5
-                dino.style.top = `${dinoTop}px`
-                jumpHeight += 5
-                isFalling = false
-            } else {
-                clearInterval(jumpInterval)
-                if (!isFalling) {
-                    isFalling = true
-                    jumpTimeout = setTimeout(startGravity, 600)
-                }
-                jumpHeight = 0
-            }
-        }, 20)
+    if (isPaused) {
+        startGame();
     }
-}
-
-function startGravity() {
-    isJumping = false
-    isFalling = true
-    canJump = false
-    let gravityInterval = setInterval(function() {
-        let dinoTop = parseInt(dino.style.top)
-        if (!isJumping && dinoTop < FLOOR_LEVEL) {
-            dinoTop += FALL_SPEED * 3
-            dino.style.top = `${dinoTop}px`
-        } else {
-            clearInterval(gravityInterval)
-            clearTimeout(jumpTimeout)
-            jumpHeight = 0
-        }
-        if (dinoTop >= FLOOR_LEVEL) {
-            dino.removeAttribute('style')
-        }
-    }, 10)
+    isJumping = true;
+    dino.style.animation = 'none';
+    setTimeout(() => {
+        dino.style.animation = 'jump 1s ease';
+    }, 100);
+    setTimeout(() => {
+        isJumping = false;
+    }, 500);
 }
 
 function sneak() {
-    if (parseInt(dinoStyle.getPropertyValue('top')) == FLOOR_LEVEL) {
-        dino.classList.remove("default")
-        dino.classList.add("sneak")
+    if (!isJumping) {
+        dino.classList.remove('default');
+        dino.classList.add('sneak');
     }
 }
 
 function resetDino() {
-    dino.classList.remove("sneak")
-    dino.classList.add("default")
+    dino.classList.remove('sneak');
+    dino.classList.add('default');
 }
